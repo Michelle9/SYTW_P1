fe7c71a7827c28ccac0a3e7400bcd8323e03ae61
Michelle9 <jhoanmary@gmail.com>
2014-09-25 00:49:29 +0100

    Corrigiendo ordenacion

--- twitter.rb
+++ twitter.rb
@@ -16,11 +16,11 @@ post '/' do
   client = my_twitter_client() 
   @name = params[:firstname] || ''
   @number = params[:n].to_i
-  @number = 1 if @number <= 0
+  @number = 10 if @number > 10
   if client.user? @name 
     amigos = client.friends(@name).take(@number)
     amigos = amigos.map { |i| [i.name, i.followers_count]}
-    @seguidores = amigos.sort_by!{ | x,y | -y}
+    @seguidores = amigos.sort_by { | x,y | -y}
   end      
   erb :twitter
 end
